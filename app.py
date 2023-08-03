d05dec1347d0083045408a0d000b0b0ad323e5fb
dr-phil673 <141211581+dr-phil673@users.noreply.github.com>
2023-08-03 08:59:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def imageInput(device, src):
             img_ = Image.open(outputpath)
             with col2:
                 st.image(img_, caption='Model Prediction(s)', use_column_width='always')
-            x, trash  = pred.split("Speed")
+            x, trash  = str(pred).split("Speed")
             st.write(x)
 
     elif src == 'From test set.':
