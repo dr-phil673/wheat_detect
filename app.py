1dd0eab160442e7c12f603e0528bbebb42a264fd
dr-phil673 <141211581+dr-phil673@users.noreply.github.com>
2023-08-03 09:29:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ def imageInput(device, src):
                 st.image(img, caption='Uploaded Image', use_column_width='always')
             ts = datetime.timestamp(datetime.now())
             imgpath = os.path.join('data/uploads', str(ts) + image_file.name)
-            outputpath = os.path.join('data/outputs', os.path.basename(imgpath))
-            ##with open(imgpath, mode="wb") as f:
-              #  f.write(image_file.getbuffer())
+            #outputpath = os.path.join('data/outputs', os.path.basename(imgpath))
+            with open(imgpath, mode="wb") as f:
+                f.write(image_file.getbuffer())
 
             # call Model prediction--
            
