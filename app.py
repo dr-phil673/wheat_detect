7157ec848ee1e26276d86bd6e0bd66fb95f6b536
dr-phil673 <141211581+dr-phil673@users.noreply.github.com>
2023-08-03 09:08:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,10 @@ def imageInput(device, src):
             with col2:
                 st.image(img_, caption='Model Prediction(s)', use_column_width='always')
             x, trash  = str(pred).split("Speed")
-            st.write("Predictions: " + x[18:])
+            x = x[18:]
+            if "persons" in x:
+                x.replace("persons", "customers")
+            st.write("Predictions: " + x)
 
     elif src == 'From test set.':
         # Image selector slider
